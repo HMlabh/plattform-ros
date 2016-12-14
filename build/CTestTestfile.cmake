b2c63b9e9331366014616e9b2a0f1ce10d691467
HMlabh <labh@gmx.net>
2016-12-14 13:06:48 +0100

    build/ gelöscht

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/labh/plattform-ros/src
-# Build directory: /home/labh/plattform-ros/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
