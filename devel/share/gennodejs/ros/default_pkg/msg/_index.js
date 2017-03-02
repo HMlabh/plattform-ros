79a7fed253b6c87cd0503ea587c4242367921451
labh <m@g.net>
2017-03-02 12:15:07 +0100

    merge versuch 2

--- devel/share/gennodejs/ros/default_pkg/msg/_index.js
+++ devel/share/gennodejs/ros/default_pkg/msg/_index.js
@@ -1,30 +1,30 @@
 
 "use strict";
 
-let ir_ranges = require('./ir_ranges.js');
-let move_vector = require('./move_vector.js');
-let lift_speeds = require('./lift_speeds.js');
-let DummyTalk = require('./DummyTalk.js');
-let lift_pos = require('./lift_pos.js');
-let lift_goal = require('./lift_goal.js');
 let test = require('./test.js');
-let mechano_speeds = require('./mechano_speeds.js');
-let lift_endstops = require('./lift_endstops.js');
-let lift_pressure = require('./lift_pressure.js');
 let usb_ident = require('./usb_ident.js');
+let mechano_speeds = require('./mechano_speeds.js');
+let lift_goal = require('./lift_goal.js');
+let move_vector = require('./move_vector.js');
 let ultra_ranges = require('./ultra_ranges.js');
+let lift_pos = require('./lift_pos.js');
+let lift_speeds = require('./lift_speeds.js');
+let ir_ranges = require('./ir_ranges.js');
+let lift_endstops = require('./lift_endstops.js');
+let DummyTalk = require('./DummyTalk.js');
+let lift_pressure = require('./lift_pressure.js');
 
 module.exports = {
-  ir_ranges: ir_ranges,
-  move_vector: move_vector,
-  lift_speeds: lift_speeds,
-  DummyTalk: DummyTalk,
-  lift_pos: lift_pos,
-  lift_goal: lift_goal,
   test: test,
-  mechano_speeds: mechano_speeds,
-  lift_endstops: lift_endstops,
-  lift_pressure: lift_pressure,
   usb_ident: usb_ident,
+  mechano_speeds: mechano_speeds,
+  lift_goal: lift_goal,
+  move_vector: move_vector,
   ultra_ranges: ultra_ranges,
+  lift_pos: lift_pos,
+  lift_speeds: lift_speeds,
+  ir_ranges: ir_ranges,
+  lift_endstops: lift_endstops,
+  DummyTalk: DummyTalk,
+  lift_pressure: lift_pressure,
 };
