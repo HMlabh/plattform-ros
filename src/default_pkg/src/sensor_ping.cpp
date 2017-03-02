dba584aafa1dd193897574f0cd1f381df96bd2ac
labh <m@g.net>
2017-03-02 15:46:41 +0100

    sensor_ping connection...

--- src/default_pkg/src/sensor_ping.cpp
+++ src/default_pkg/src/sensor_ping.cpp
@@ -2,6 +2,7 @@
 #include "std_msgs/String.h"
 #include "std_msgs/Int8.h"
 #include "default_pkg/ultra_ranges.h"
+#include "stdlib.h"
 
 #include <unistd.h>
 #include <iostream>
@@ -11,6 +12,15 @@
 
 using namespace LibSerial;
 
+char ask_ident = 'i'; //"i"
+char got_ident;
+char call = 'c';
+char ask = 'a';
+char askram[32];
+
+char trash;
+int8_t testchartoint;
+
 int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "sensor_ping");
@@ -18,10 +28,12 @@ int main(int argc, char **argv)
 
 	//creating Publisher
 	ros::Publisher sensor_ping_pub = n.advertise<default_pkg::ultra_ranges>("sensor_ping_out", 10);
-	ros::Rate loop_rate(1);
+	ros::Rate loop_rate(100);
 
 	//open serial connection 
 	SerialStream Arduino_Serial;
+
+	ROS_INFO("connecting...");
 
 	Arduino_Serial.Open("/dev/ttyACM0");
 	Arduino_Serial.SetBaudRate(SerialStreamBuf::BAUD_115200);
@@ -29,34 +41,46 @@ int main(int argc, char **argv)
 	Arduino_Serial.SetNumOfStopBits(1);
 	Arduino_Serial.SetFlowControl(SerialStreamBuf::FLOW_CONTROL_HARD);
 
+	ROS_INFO("connected");
 
-	int out = 42;
+	char ask_ident = 'i'; //"i"
+		char got_ident;
 
-	for (int in = 32; in < 64; in++)
-	{
-		std::cout << "in: " << in;
-		Arduino_Serial << in;
-		//mySerial >> out;
-		std::cout << "\t→ out: " << out << std::endl;
-		sleep(1);
-	}
-	
+		Arduino_Serial << ask_ident;
 
+		Arduino_Serial >> got_ident;
 
-	//------
+		ROS_INFO("ID: %c", got_ident);
 
-	/**
-	 * A count of how many messages we have sent. This is used to create
-	 * a unique string for each message.
-	 */
-
-
+	ROS_INFO("complete");
 	int count = 0;
 	while (ros::ok())
 	{
-		/**
-		 * This is a message object. You stuff it with data, and then publish it.
-		 */
+
+
+		//asking;
+		Arduino_Serial << call;
+		usleep(20000);
+		Arduino_Serial << ask;
+		usleep(40000);
+		
+
+	
+		Arduino_Serial.read(askram,32);
+
+		int16_t testtest = 0;
+		testtest = (((int16_t)askram[0]) | ((int16_t)(askram[1]<<8)));
+
+		ROS_INFO("LSB: %d", (int)askram[0]);
+		ROS_INFO("MSB: %d", (int)askram[1]);
+
+		ROS_INFO("%d", testtest);
+
+
+
+
+
+
 		::default_pkg::ultra_ranges ultra_r;
 		ultra_r.range_su0 = 0.4;
 		ultra_r.range_su1 = 44.0;
@@ -68,15 +92,9 @@ int main(int argc, char **argv)
 		ultra_r.range_su7 = 22.4;
 		ultra_r.range_su8 = 22.4;
 		//ultra_r.range_su9 = 99;
+		
 
-		int out = 73; //"I"
-		int in =0;
-
-		Arduino_Serial >> out;
-
-		Arduino_Serial << in;
-
-		ROS_INFO("%d", in);
+		
 
 		sensor_ping_pub.publish(ultra_r);
 		ros::spinOnce();
