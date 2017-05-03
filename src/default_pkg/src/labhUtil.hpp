1ce1bda5adf37265992bb949cd1fbcf649e2ee66
labh <m@g.net>
2017-05-03 17:37:25 +0200

    class for usb-msg #krl

--- src/default_pkg/src/labhUtil.hpp
+++ src/default_pkg/src/labhUtil.hpp
@@ -20,27 +20,45 @@
 #include <serial/serial.h>
 #include <SerialStream.h>
 
+#define SIZE_USB_MSG 9
+
+
+
+
 class labhUtil
 {
 	public:
-// debug foo
+		// debug foo
 		void test(void);
-// getter/setter
+		// getter/setter
 		int getIdent();
 
-// usefull stuff
+		// usefull stuff
 		void usbIdent(int usbIdent);
 		std::string getUSBloc();
 		int startSerial(void);
 		void stopSerial(void);
 		void writeChar(char c);
-// varibles
+
+
+		// varibles
+
 	private:
 		int _ident;
 		std::string _port;
 		int _baud;
 		LibSerial::SerialStream _Serial;
 
+};
+
+class labhUtilUsbMsg
+{
+	public:
+		struct data {
+			int idents[SIZE_USB_MSG];
+			std::string locs[SIZE_USB_MSG];
+		};
+		data foo;
 };
 
 #endif
--- src/default_pkg/src/mechano_speeds.cpp
+++ src/default_pkg/src/mechano_speeds.cpp
@@ -11,6 +11,7 @@
 #include "ros/ros.h"
 #include "std_msgs/Int16.h"
 #include "default_pkg/mechano_speeds.h"
+#include "default_pkg/usb_ident.h"
 
 #include <stdlib.h>
 #include <string.h>
@@ -29,6 +30,39 @@ using namespace LibSerial;
 
 SerialStream MechanoSerial;
 
+labhUtilUsbMsg data;
+
+
+void usbIdentCall(const default_pkg::usb_ident::ConstPtr& msg)
+{
+	ROS_INFO("I heard: [%s]", msg->usb_loc0.c_str());
+	data.foo.idents[0] = msg->usb_ident0;
+	ROS_INFO("ident: %d", data.foo.idents[0]);
+
+/*
+	usbMsg.Data.idents[0] = msg->usb_ident0;
+	usbMsg.Data.idents[1] = msg->usb_ident1;
+	usbMsg.Data.idents[2] = msg->usb_ident2;
+	usbMsg.Data.idents[3] = msg->usb_ident3;
+	usbMsg.Data.idents[4] = msg->usb_ident4;
+	usbMsg.Data.idents[5] = msg->usb_ident5;
+	usbMsg.Data.idents[6] = msg->usb_ident6;
+	usbMsg.Data.idents[7] = msg->usb_ident7;
+	usbMsg.Data.idents[8] = msg->usb_ident8;
+	usbMsg.Data.idents[9] = msg->usb_ident9;
+
+	usbMsg.Data.locs[0] = msg->usb_loc0;
+	usbMsg.Data.locs[1] = msg->usb_loc1;
+	usbMsg.Data.locs[2] = msg->usb_loc2;
+	usbMsg.Data.locs[3] = msg->usb_loc3;
+	usbMsg.Data.locs[4] = msg->usb_loc4;
+	usbMsg.Data.locs[5] = msg->usb_loc5;
+	usbMsg.Data.locs[6] = msg->usb_loc6;
+	usbMsg.Data.locs[7] = msg->usb_loc7;
+	usbMsg.Data.locs[8] = msg->usb_loc8;
+	usbMsg.Data.locs[9] = msg->usb_loc9;
+*/
+}
 
 
 /*-------Main----------------------*/
@@ -37,9 +71,12 @@ int main( int argc, char **argv)
 	/*	Node Init	*/
 	ros::init(argc, argv, "mechano_speeds");
 	ros::NodeHandle nodeHandle;
-	
+
+	ros::Subscriber sub = nodeHandle.subscribe("usb_detect", 1000, usbIdentCall);
+
 	labhUtil foo;
 	foo.test();
+	
 	foo.usbIdent(mechanoIdent);
 	foo.getIdent();
 	ROS_INFO("myIdent = %d", foo.getIdent());
@@ -50,6 +87,7 @@ int main( int argc, char **argv)
 	/* enter spin loop:	*/
 	ROS_INFO("enter loop ....");
 	ros::Rate loop_rate(1);		// 10Hz
+
 
 	while (ros::ok())
 	{
